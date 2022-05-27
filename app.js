8426a62cab5628ef0a989929fed7ac39f66a09f5
anthoni Bortolotto Martinelli <antho@outlook.com.br>
2022-05-26 21:23:37 -0300

    cors

--- app.js
+++ app.js
@@ -5,9 +5,15 @@ const app = express();
 const index = require("./api/routes/index");
 const livrosVenda = require("./api/routes/livrosVenda");
 const user = require("./api/routes/user");
+const cors = require("cors");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use("/", index);
 app.use("/livros", livrosVenda);
