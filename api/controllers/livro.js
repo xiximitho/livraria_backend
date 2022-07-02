9c3df81734763588d03585e9862d36bdadaaa611
Felipe <mattesfelipe@gmail.com>
2022-07-02 18:26:40 -0300

    criado busca de todos livros

--- api/controllers/livro.js
+++ api/controllers/livro.js
@@ -8,12 +8,23 @@ const pool = new Pool({
   port: 5432,
 });
 
+exports.getAll = (req, res) => {
+    pool.query(
+      "SELECT * FROM livros where genre is not null and img is not null order by rating desc limit 100 ",
+      (error, results) => {
+        if (error) {
+          throw error;
+        }
+        res.status(200).json(results.rows);
+      }
+    );
+  };
 
 exports.getByTitulo = (req, res) => {
   const titulo = req.params.livro;
   console.log(titulo)
   pool.query(
-    "SELECT * FROM livros where title like '%"+titulo+"%'",
+    "SELECT * FROM livros where title like '%"+titulo+"%' and genre is not null and img is not null limit 100 ",
     (error, results) => {
       if (error) {
         throw error;
@@ -23,3 +34,30 @@ exports.getByTitulo = (req, res) => {
   );
 };
 
+exports.getByGenre = (req, res) => {
+    const genero = req.params.genre;
+    console.log(genero)
+    pool.query(
+      "SELECT * FROM livros where genre like '%"+genero+"%' and genre is not null and img is not null and title is not null limit 100",
+      (error, results) => {
+        if (error) {
+          throw error;
+        }
+        res.status(200).json(results.rows);
+      }
+    );
+  };
+
+  exports.getByRate = (req, res) => {
+    const rate = req.params.rate;
+    console.log(rate)
+    pool.query(
+      "SELECT * FROM livros where rating > "+rate+ " and genre is not null and img is not null and title is not null limit 100",
+      (error, results) => {
+        if (error) {
+          throw error;
+        }
+        res.status(200).json(results.rows);
+      }
+    );
+  };
--- api/routes/livro.js
+++ api/routes/livro.js
@@ -3,5 +3,9 @@ const router = express.Router();
 const controller = require("../controllers/livro")
 
 router.get("/get/:livro", controller.getByTitulo);
+router.get("/get/", controller.getAll);
+router.get("/get/livro/genre/:genre", controller.getByGenre);
+router.get("/get/livro/rate/:rate", controller.getByRate);
+
 
 module.exports = router;
