ee33726d296d0001420e5531c5a52a14a9408a87
anthoni Bortolotto Martinelli <antho@outlook.com.br>
2022-09-09 19:40:40 -0300

    port fix

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const setApp = require('./src')
 setApp
   .then((app) =>
-    app.listen(3000, () => console.info('app running on port 3000'))
+    app.listen(3001, () => console.info('app running on port 3001'))
   )
   .catch((error) => {
     console.error(error)
